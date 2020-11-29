03583be3e185d418b85068df4e5de014feb99ee8
gutoalvim <gutoalvim@hotmail.com>
2020-11-29 02:07:34 -0300

    thy first commit

--- femder/__init__.py
+++ femder/__init__.py
@@ -4,6 +4,11 @@ Created on Sat Nov 28 00:24:04 2020
 
 @author: gutoa
 """
-from MENAV.controlsair import AirProperties, AlgControls, sph2cart, cart2sph
-from MENAV.FEM_1D import FEM1D
-from MENAV.grid_importing import GridImport, GridImport3D
+from femder.controlsair import AirProperties, AlgControls, sph2cart, cart2sph
+from femder.FEM_1D import FEM1D
+from femder.grid_importing import GridImport, GridImport3D
+from femder.FEM_3D import FEM3D
+from femder.BoundaryConditions import BC
+from femder.TMM_rina_improved import TMM
+from femder.sources import Source
+from femder.receivers import Receiver
