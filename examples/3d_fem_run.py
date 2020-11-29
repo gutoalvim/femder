29741b108878ae3bae15e9e154d29440c248bc28
gutoalvim <gutoalvim@hotmail.com>
2020-11-29 19:27:28 -0300

    addded gitignore

---
+++ examples/3d_fem_run.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 28 00:30:38 2020
+
+@author: gutoa
+"""
+import femder as fd
+import numpy as np
+
+path_to_geo = "..\\Mshs\\FEM_3D\\cplx_room.geo"
+
+
+AP = fd.AirProperties(c0 = 343)
+AC = fd.AlgControls(AP,200,200,1)
+
+S = fd.Source("spherical")
+S.coord = np.array([[-1,2.25,1.2],[1,2.25,1.2]])
+S.q = np.array([[0.0001],[0.0001]])
+
+R = fd.Receiver([0,1,1.2])
+
+BC = fd.BC(AC,AP)
+BC.normalized_admittance(2,0.02)
+BC.rigid(3)
+
+grid = fd.GridImport3D(AP,path_to_geo, fmax=200, num_freq=6, plot=False,scale=1)
+
+obj = fd.FEM3D(grid,S,AP,AC,BC)
+#%%
+obj.compute()
+pN = obj.evaluate(R,plot=True)
+
+#%%
+
